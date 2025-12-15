f31238c8e6a879e358c1d319e281f1af734a5de1
Simon Zhao <simonzhao219@gmail.com>
2025-12-16 06:42:43 +0800

    feat: Add detailed meal information with traceability and operator data

---
+++ lib/data/three_label_one_code.dart
@@ -0,0 +1,487 @@
+Map<String, Map<String, String>> idToMealName = {
+  "甘藷（地瓜）": {
+    "traceability_code": "1119478547778",
+    "operator": "地瓜王",
+    "packaging_date": "2025/10/31",
+    "phone": "037887888",
+    "address": "苗栗縣後龍鎮地瓜段"
+  },
+  "糙米": {
+    "traceability_code": "1152002200002",
+    "operator": "陳水稻",
+    "packaging_date": "2025/10/31",
+    "phone": "042002002",
+    "address": "彰化縣溪州鄉好米段"
+  },
+  "杏鮑菇": {
+    "traceability_code": "1117778889999",
+    "operator": "菇小弟",
+    "packaging_date": "2025/10/31",
+    "phone": "047778888",
+    "address": "彰化縣溪州鄉好菇段"
+  },
+  "白米": {
+    "traceability_code": "1152002200001",
+    "operator": "陳水稻",
+    "packaging_date": "2025/10/31",
+    "phone": "042002002",
+    "address": "彰化縣溪州鄉好米段"
+  },
+  "高麗菜": {
+    "traceability_code": "2202002200005",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "胡蘿蔔": {
+    "traceability_code": "2202002200006",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "小黃瓜": {
+    "traceability_code": "2202002200007",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "結頭菜": {
+    "traceability_code": "2202002200008",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "白蘿蔔": {
+    "traceability_code": "2202002200009",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "山藥": {
+    "traceability_code": "2025333113131",
+    "operator": "土地下面",
+    "packaging_date": "2025/10/31",
+    "phone": "047733333",
+    "address": "彰化縣埔鹽鄉好土段"
+  },
+  "花生": {
+    "traceability_code": "2000333113133",
+    "operator": "土豆伯",
+    "packaging_date": "2025/10/31",
+    "phone": "057774444",
+    "address": "雲林縣虎尾鎮土豆路段"
+  },
+  "牛蕃茄": {
+    "traceability_code": "2202002200911",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "059996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "綠豆芽": {
+    "traceability_code": "2210010100100",
+    "operator": "鄭好芽",
+    "packaging_date": "2025/10/31",
+    "phone": "033396969",
+    "address": "桃園市八德區好芽段"
+  },
+  "黃豆芽": {
+    "traceability_code": "2210010100101",
+    "operator": "鄭好芽",
+    "packaging_date": "2025/10/31",
+    "phone": "033396969",
+    "address": "桃園市八德區好芽段"
+  },
+  "白花菜": {
+    "traceability_code": "2025333777444",
+    "operator": "土地上面",
+    "packaging_date": "2025/10/31",
+    "phone": "047733333",
+    "address": "彰化縣埔鹽鄉好土段"
+  },
+  "洋蔥": {
+    "traceability_code": "2202002200912",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "049996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "芹菜": {
+    "traceability_code": "2202002200913",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "049996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "豆漿": {
+    "traceability_code": "4999049990823",
+    "operator": "島拎",
+    "packaging_date": "2025/10/31",
+    "phone": "053344444",
+    "address": "嘉義縣中埔鄉大豆路段"
+  },
+  "大白菜": {
+    "traceability_code": "2202002200914",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "049996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "綠竹筍": {
+    "traceability_code": "2212012210901",
+    "operator": "金好筍",
+    "packaging_date": "2025/10/31",
+    "phone": "022299404",
+    "address": "新北市五股區好筍段"
+  },
+  "馬鈴薯": {
+    "traceability_code": "2202002200918",
+    "operator": "蔬菜王",
+    "packaging_date": "2025/10/31",
+    "phone": "049996666",
+    "address": "雲林縣北港鎮好菜段"
+  },
+  "文蛤": {
+    "traceability_code": "2220001100324",
+    "operator": "蛤蠣王",
+    "packaging_date": "2025/10/31",
+    "phone": "063237787",
+    "address": "臺南市七股區好蛤段"
+  },
+  "莧菜(含綠莧、白莧、紅莧)": {
+    "category": "短期葉菜",
+    "operator": "葉菜成（葉菜有機農場）",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575702",
+    "expiration_date": "2026/10/10",
+    "address": "雲林縣莿桐鄉蔬菜段5757-0000地號 ",
+    "phone": "091777777"
+  },
+  "青松菜": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "菠菜": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "地瓜葉": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "小松菜": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "空心菜": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "小白菜": {
+    "category": "短期葉菜",
+    "operator": "有機蔬菜有限公司",
+    "certification_body": "國立中興大學",
+    "certificate_number": "1-006-575703",
+    "expiration_date": "2026/10/10",
+    "address": "臺中市大雅區有機路3號5樓",
+    "phone": "048884444"
+  },
+  "鮮乳": {
+    "category": "乳品",
+    "manufacturer": "新鮮乳廠",
+    "address": "桃園市大園區鮮乳路99號",
+    "phone": "0377775555",
+    "representative": "牛奶鮮",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "150666",
+    "product_type": "鮮乳"
+  },
+  "雞蛋": {
+    "category": "生鮮蛋品",
+    "manufacturer": "新鮮蛋場",
+    "address": "彰化縣竹塘鄉好蛋路6號",
+    "phone": "048887777",
+    "representative": "王好蛋",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "110888",
+    "product_type": "生鮮蛋品"
+  },
+  "冷凍鬼頭刀丁": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168168",
+    "product_type": "冷凍水產品"
+  },
+  "雞胸肉": {
+    "category": "肉品",
+    "manufacturer": "新鮮肉廠",
+    "address": "台南市柳營區雞肉路10號",
+    "phone": "075558888",
+    "representative": "初雞肉",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "092403",
+    "product_type": "冷藏雞肉"
+  },
+  "豬後腿肉": {
+    "category": "肉品",
+    "manufacturer": "新鮮豬廠",
+    "address": "雲林縣大埤鄉豬豬路100號",
+    "phone": "055555999",
+    "representative": "朱肉讚",
+    "certification_body": "財團法人台灣優良農產品發展協會",
+    "certification_id": "092404",
+    "product_type": "冷凍豬肉"
+  },
+  "冷凍鯛魚片": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168169",
+    "product_type": "冷凍水產品"
+  },
+  "肉片": {
+    "category": "肉品",
+    "manufacturer": "新鮮豬廠",
+    "address": "雲林縣大埤鄉豬豬路100號",
+    "phone": "055555999",
+    "representative": "朱肉讚",
+    "certification_body": "財團法人台灣優良農產品發展協會",
+    "certification_id": "092405",
+    "product_type": "冷凍豬肉"
+  },
+  "雞腿肉": {
+    "category": "肉品",
+    "manufacturer": "新鮮肉廠",
+    "address": "台南市柳營區雞肉路10號",
+    "phone": "075558888",
+    "representative": "初雞肉",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "092403",
+    "product_type": "冷藏雞肉"
+  },
+  "小卷（鎖管）": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168170",
+    "product_type": "冷凍水產品"
+  },
+  "虱目魚肚": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168172",
+    "product_type": "冷凍水產品"
+  },
+  "五花肉": {
+    "category": "肉品",
+    "manufacturer": "新鮮豬廠",
+    "address": "雲林縣大埤鄉豬豬路100號",
+    "phone": "055555999",
+    "representative": "朱肉讚",
+    "certification_body": "財團法人台灣優良農產品發展協會",
+    "certification_id": "092406",
+    "product_type": "冷凍豬肉"
+  },
+  "冷凍虱目魚": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168173",
+    "product_type": "冷凍水產品"
+  },
+  "三節翅": {
+    "category": "肉品",
+    "manufacturer": "新鮮肉廠",
+    "address": "台南市柳營區雞肉路10號",
+    "phone": "075558888",
+    "representative": "初雞肉",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "092407",
+    "product_type": "冷藏雞肉"
+  },
+  "冷凍石斑魚": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168173",
+    "product_type": "冷凍水產品"
+  },
+  "冷凍秋刀魚": {
+    "category": "水產加工品",
+    "manufacturer": "新鮮水產",
+    "address": "屏東縣東港鎮大船路6號",
+    "phone": "088787878",
+    "representative": "郝吃魚",
+    "certification_body": "財團法人食品工業發展研究所",
+    "certification_id": "168174",
+    "product_type": "冷凍水產品"
+  },
+  "梅花豬柳條": {
+    "category": "肉品",
+    "manufacturer": "新鮮豬廠",
+    "address": "雲林縣大埤鄉豬豬路100號",
+    "phone": "055555999",
+    "representative": "朱肉讚",
+    "certification_body": "財團法人台灣優良農產品發展協會",
+    "certification_id": "092408",
+    "product_type": "冷凍豬肉"
+  },
+  "白色肉雞：雞排": {
+    "category": "肉品",
+    "manufacturer": "新鮮肉廠",
+    "address": "台南市柳營區雞肉路10號",
+    "phone": "075558888",
+    "representative": "初雞肉",
+    "certification_body": "財團法人中央畜產會",
+    "certification_id": "092409",
+    "product_type": "冷藏雞肉"
+  },
+  "葡萄": {
+    "trace_code": "095279527",
+    "producer": "呂葡萄-葡萄公主莊園",
+    "phone": "048889527",
+    "address": "臺中市新社區",
+    "description": "獲得家鄉葡萄公主的殊榮，傳承父執輩高品質的種植技術，分享大家。"
+  },
+  "毛豆": {
+    "trace_code": "099887766",
+    "producer": "毛豆贊",
+    "phone": "068866168",
+    "address": "嘉義縣中埔鄉",
+    "description": "為了下一代的健康，種植出讓自己也安心吃下肚的方式種植。"
+  },
+  "小米": {
+    "trace_code": "101101101",
+    "producer": "米家好",
+    "phone": "044445678",
+    "address": "臺東縣台東市",
+    "description": "小米曾經是重要的糧食作物，秉持著具有文化性和作物的基因多樣性意義，繼續種下去。"
+  },
+  "南瓜": {
+    "trace_code": "780780999",
+    "producer": "在地菜農",
+    "phone": "048889526",
+    "address": "臺中市西屯區",
+    "description": "提供在地人來自在地的食材"
+  },
+  "櫛瓜": {
+    "trace_code": "780780999",
+    "producer": "在地菜農",
+    "phone": "048889526",
+    "address": "臺中市西屯區",
+    "description": "提供在地人來自在地的食材"
+  },
+  "蓮藕": {
+    "trace_code": "780780888",
+    "producer": "藕在這",
+    "phone": "033335567",
+    "address": "桃園市觀音區",
+    "description": "農地位於桃園觀音區種植蓮藕、蔬菜等農作物。"
+  },
+  "白木耳": {
+    "trace_code": "556655778",
+    "producer": "菇菇好",
+    "phone": "049996699",
+    "address": "彰化縣和美鎮",
+    "description": "菇菇深耕在本地農業供應鏈，擁有多年養菇經驗，秉持著「安心、安全、創新」的經營理念，希望每個客戶，都能享受到來自土地的恩惠。"
+  },
+  "彩色甜椒": {
+    "trace_code": "334455678",
+    "producer": "吾農",
+    "phone": "083344567",
+    "address": "屏東縣里港鄉",
+    "description": "務農五十多年的經驗，師承自然，尊重大自然的心意從吾而農。"
+  },
+  "芭樂": {
+    "trace_code": "886888668",
+    "producer": "水果大王",
+    "phone": "058686868",
+    "address": "雲林縣虎尾鎮",
+    "description": "在台灣這塊寶島上種植水果讓國民享用，是我的幸福，讓吃我的水果的大家也幸福。"
+  },
+  "火龍果": {
+    "trace_code": "886888668",
+    "producer": "水果大王",
+    "phone": "058686868",
+    "address": "雲林縣虎尾鎮",
+    "description": "在台灣這塊寶島上種植水果讓國民享用，是我的幸福，讓吃我的水果的大家也幸福。"
+  },
+  "香蕉": {
+    "trace_code": "777799999",
+    "producer": "金蕉世家",
+    "phone": "087779999",
+    "address": "屏東縣高樹鄉",
+    "description": "在北回歸線以南種植的香蕉，吸取最多的太陽精華，長成金黃香蕉。"
+  },
+  "黃豆": {
+    "trace_code": "156801680",
+    "producer": "豆抵家",
+    "phone": "092738383",
+    "address": "臺南市下營區",
+    "description": "原先我是為了增加土壤氮肥而種植大豆，如今，則希望藉著種植國產非基改大豆，讓大家吃得更安心。"
+  },
+  "小番茄": {
+    "trace_code": "886888669",
+    "producer": "水果大王",
+    "phone": "058686868",
+    "address": "雲林縣虎尾鎮",
+    "description": "在台灣這塊寶島上種植水果讓國民享用，是我的幸福，讓吃我的水果的大家也幸福。"
+  },
+  "紅豆": {
+    "trace_code": "777766666",
+    "producer": "晶晶豆豆",
+    "phone": "077777666",
+    "address": "高雄市大寮區",
+    "description": "和家人一起在家鄉種植，以自家也能安心食用為初衷，所有作物皆接受嚴格把關，主力生產大豆和紅豆。"
+  }
+};
